d7166f9ab5afe2d4dd41aa8892f578a424743c98
vinodkumarDV <dvvinod143@gmail.com>
2021-12-29 20:26:56 +0530

    merged 2 component in 1 page and did some crud operation

--- component/Input.js
+++ component/Input.js
@@ -1,53 +1,86 @@
-import React, { Component} from 'react'
-import { TextInput, View, StyleSheet, Button } from 'react-native'
+import React, { Component } from "react";
+import { TextInput, View, StyleSheet, Button } from "react-native";
+import UploadProdect from './UploadProdect'
 
 export class Input extends Component {
     constructor() {
-        super(props);
+        super();
         this.state = {
             title: " ",
             price: " ",
-            description: " ",
-        }
+            description: " "
+        };
     }
     Submit() {
-        console.log(this.state)
+        console.log(this.state);
     }
 
     render() {
         return (
-            < View style = { styles.container } >
-                <TextInput 
+            <View style={styles.container}>
+                <UploadProdect/>
+                <TextInput
                     placeholder="  enter title"
-                    onChange={(text) => { this.setState({ title: text }) }}
-                    style={{ borderWidth: 3, borderColor: 'black', marginStart: 11, width: 340, height: 50, marginBottom: 5, borderRadius: 5 }}
+                    onChange={(text) => {
+                        this.setState({ title: text });
+                    }}
+                    style={{
+                        borderWidth: 3,
+                        borderColor: "black",
+                        width: "95%",
+                        height: 50,
+                        marginBottom: 5,
+                        borderRadius: 5
+                    }}
                 />
-                <TextInput 
+                <TextInput
                     placeholder="  enter price"
-                    onChange={(text) => { this.setState({ price: text }) }}
-                    style={{ borderWidth: 3, borderColor: 'black', marginStart: 11, width: 340, height: 50, marginBottom: 5, borderRadius: 5 }}
+                    onChange={(text) => {
+                        this.setState({ price: text });
+                    }}
+                    style={{
+                        borderWidth: 3,
+                        borderColor: "black",
+                        width: '95%',
+                        height: 50,
+                        marginBottom: 5,
+                        borderRadius: 5
+                    }}
                 />
-                <TextInput 
+                <TextInput
                     placeholder="  enter description"
-                    onChange={(text) => { this.setState({ description: text }) }}
-                    style={{ borderWidth: 3, borderColor: 'black', marginStart: 11, width: 340, height: 50, marginBottom: 5, borderRadius: 5, alignItems: 'center'}}
+                    onChange={(text) => {
+                        this.setState({ description: text });
+                    }}
+                    style={{
+                        borderWidth: 3,
+                        borderColor: "black",
+                        width: '95%',
+                        height: 50,
+                        marginBottom: 5,
+                        borderRadius: 5,
+                        alignItems: "center"
+                    }}
                 />
-                <Button title='submit' onPress={() => { this.Submit() }}
-                    
+                <Button
+                    title="submit"
+                    onPress={() => {
+                        this.Submit();
+                    }}
                 />
             </View>
-            
-        )
+        );
     }
 }
 
-export default Input
+export default Input;
 
 const styles = StyleSheet.create({
     container: {
-        display: 'flex',
-        backgroundColor: 'white',
-        alignItems: 'center',
-        justifyContent: 'center',
+        display: "flex",
+        backgroundColor: "white",
+        alignItems: "center",
+        justifyContent: "center",
+        width: '100%',
     }
-})
+});
