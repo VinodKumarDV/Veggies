ed4f9f09b160d72392b7d7ecfaea3fd8430ea3c2
SoulReaper496 <praveendassking@gmail.com>
2021-12-31 09:24:12 +0530

    Updated DetailsScreen

--- Components/DetailsScreen.js
+++ Components/DetailsScreen.js
@@ -1,6 +1,7 @@
 import React from "react";
-import { View, Text, SafeAreaView, StyleSheet } from "react-native";
+import { View, Text, SafeAreaView, StyleSheet, Image } from "react-native";
 import COLORS from "../src/const/color";
+import Veggie from "../src/const/Veggie";
 import {
   FontAwesome,
   MaterialIcons,
@@ -21,6 +22,88 @@ const DetailsScreen = ({ navigation, route }) => {
         />
         <AntDesign name="shoppingcart" size={28} />
       </View>
+      <View style={styles.imageContainer}>
+        <Image source={veggie.img} style={{ resizeMode: "contain", flex: 1 }} />
+      </View>
+      <View style={styles.detailsCotainer}>
+        <View
+          style={{
+            marginLeft: 20,
+            flexDirection: "row",
+            alignItems: "flex-end",
+          }}
+        >
+          <View style={styles.line} />
+          <Text style={{ fontSize: 18, fontWeight: "bold" }}>Best Choice</Text>
+        </View>
+        <View
+          style={{
+            marginLeft: 20,
+            marginTop: 20,
+            flexDirection: "row",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
+          <Text style={{ fontSize: 22, fontWeight: "bold" }}>
+            {veggie.name}
+          </Text>
+          <View style={styles.priceTag}>
+            <Text
+              style={{
+                marginLeft: 20,
+                fontWeight: "bold",
+                fontSize: 16,
+                color: COLORS.white,
+              }}
+            >
+              Rs{veggie.price}
+            </Text>
+          </View>
+        </View>
+        <View style={{ paddingHorizontal: 20, marginTop: 10 }}>
+          <Text style={{ fontSize: 20, fontWeight: "bold" }}>About</Text>
+          <Text
+            style={{
+              color: "grey",
+              fontSize: 16,
+              lineHeight: 22,
+              marginTop: 20,
+            }}
+          >
+            {veggie.about}
+          </Text>
+        </View>
+        <View
+          style={{
+            marginTop: 20,
+            flexDirection: "row",
+            justifyContent: "space-between",
+          }}
+        >
+          <View style={{ flexDirection: "row", alignItems: "center" }}>
+            <View style={styles.borderBtn}>
+              <Text style={styles.borderBtnText}>-</Text>
+            </View>
+            <Text
+              style={{ fontWeight: "bold", fontSize: 20, marginHorizontal: 10 }}
+            >
+              {" "}
+              1
+            </Text>
+            <View style={styles.borderBtn}>
+              <Text style={styles.borderBtnText}>+</Text>
+            </View>
+          </View>
+          <View style={styles.buyBtn}>
+            <Text
+              style={{ fontSize: 20, fontWeight: "bold", color: COLORS.white }}
+            >
+              Buy
+            </Text>
+          </View>
+        </View>
+      </View>
     </SafeAreaView>
   );
 };
@@ -31,6 +114,57 @@ const styles = StyleSheet.create({
     marginTop: 20,
     flexDirection: "row",
     justifyContent: "space-between",
+  },
+  imageContainer: {
+    flex: 0.45,
+    marginTop: 20,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  detailsCotainer: {
+    flex: 0.55,
+    backgroundColor: COLORS.light,
+    marginHorizontal: 7,
+    marginBottom: 7,
+    borderRadius: 20,
+    marginTop: 20,
+    paddingTop: 20,
+  },
+  line: {
+    width: 25,
+    height: 2,
+    backgroundColor: COLORS.dark,
+    marginBottom: 5,
+    marginRight: 3,
+  },
+  priceTag: {
+    backgroundColor: COLORS.green,
+    width: 80,
+    height: 40,
+    borderTopLeftRadius: 25,
+    borderBottomLeftRadius: 25,
+    justifyContent: "center",
+  },
+  borderBtn: {
+    borderColor: "grey",
+    borderWidth: 1,
+    borderRadius: 5,
+    height: 40,
+    width: 50,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  borderBtnText: {
+    fontWeight: "bold",
+    fontSize: 28,
+  },
+  buyBtn: {
+    backgroundColor: COLORS.green,
+    height: 50,
+    width: 150,
+    borderRadius: 25,
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
 export default DetailsScreen;
--- src/const/Veggie.js
+++ src/const/Veggie.js
@@ -2,31 +2,34 @@ const veggie = [
   {
     id: 1,
     name: "Tomato",
-    price: "20",
+    price: 20,
     like: true,
     img: require("../assets/Tomato.jpg"),
-    about: "This is a Hybrid Toamto ",
+    about:
+      "Tomatoes contain Vitamin C,K. lycopene, an antioxidant that reduces the risk of cancer and heart-diseases. They protect the eyes from light induced damage. ",
   },
   {
     id: 2,
     name: "Potato",
-    price: "40",
+    price: 40,
     like: false,
     img: require("../assets/potato.jpg"),
-    about: "This is a Country Potato ",
+    about:
+      " If you are looking for soft, slightly sweet but creamy-textured potatoes. These are a special variant early harvest of potatoes (not to be confused with our regular Fresho Potato) and are easily distinguishable with their thin/ tender skin which makes them easy-to-peel off even it can be used without peeling. These freshly picked potatoes are a great choice for roasting or boiling",
   },
   {
     id: 3,
     name: "Onion",
-    price: "10",
+    price: 10,
     like: false,
     img: require("../assets/onion.jpg"),
-    about: "This is a Hybrid Onion ",
+    about:
+      "Onions are known to be rich in biotin. Most of the flavonoids which are known as anti-oxidants are concentrated more in the outer layers, so when you peel off the layers, you should remove as little as possible. ",
   },
   {
     id: 4,
     name: "Country Onion",
-    price: "30",
+    price: 30,
     like: true,
     img: require("../assets/countryOnion.jpg"),
     about: "This is a Hybrid CountryOnion",
@@ -34,7 +37,7 @@ const veggie = [
   {
     id: 5,
     name: "Garlic",
-    price: "50",
+    price: 50,
     like: false,
     img: require("../assets/garlic.jpg"),
     about: "Fine Garlic From CountrySide ",
@@ -42,7 +45,7 @@ const veggie = [
   {
     id: 6,
     name: "Ginger",
-    price: "60",
+    price: 60,
     like: false,
     img: require("../assets/garlic.jpg"),
     about: "Fine garlic for Better Health ",
@@ -50,7 +53,7 @@ const veggie = [
   {
     id: 7,
     name: "DrumStick",
-    price: "30",
+    price: 30,
     like: false,
     img: require("../assets/drumStick.jpg"),
     about: "This is a Hybrid Deumstick",
@@ -58,7 +61,7 @@ const veggie = [
   {
     id: 8,
     name: "Carrot",
-    price: "25",
+    price: 25,
     like: true,
     img: require("../assets/carrot.jpg"),
     about: "This is a Ooty Carrot ",
@@ -66,7 +69,7 @@ const veggie = [
   {
     id: 9,
     name: "BeetRoot",
-    price: "50",
+    price: 50,
     like: true,
     img: require("../assets/Veg.jpg"),
     about: "BeetRoot ",
